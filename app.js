ee12591e300e3853d5d53ac24e9c8e4061d99aa8
Chris Perez <chris.k.perez@gmail.com>
2016-03-23 14:20:12 -0700

    more refactoring. Have 1 table, need loops for more

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var ballard, firstHill, international, sLU, georgetown, ravenna;
+
 //random number function used to generate pizza sold and delivered numbers
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -12,7 +14,7 @@ function HourlySalesData(time, minSold, maxSold, minDelivered, maxDelivered){
   this.time = time;
   this.pizzaSold = getRandomIntInclusive(minSold, maxSold);
   this.pizzaDelivered = getRandomIntInclusive(minDelivered, maxDelivered);
-  this.driversNeeded = Math.ceil(this.pizzaDelivered / 3);
+  this.driversNeeded = Math.ceil(this.pizzaDelivered / 3); //this works because it is below this.pizzaDelivered
 }
 
 //method on the prototype of PizzaLocation to push HourlySalesData into array
@@ -20,7 +22,7 @@ PizzaLocation.prototype.pushHourlySalesData = function(data){
   this.hourlySalesData.push(data);
 };
 //creating Ballard with constructor
-var ballard = new PizzaLocation('Ballard');
+ballard = new PizzaLocation('Ballard'); //now ballard is an object with a name and an empty array at ballard.hourlySalesData
 //calling the pushHourlySalesData method with completed HourlySalesData constructor as data
 ballard.pushHourlySalesData(new HourlySalesData('8:00 am', 0, 3, 1, 7));
 ballard.pushHourlySalesData(new HourlySalesData('9:00 am', 0, 3, 1, 7));
@@ -42,7 +44,7 @@ ballard.pushHourlySalesData(new HourlySalesData('12:00 am', 8, 15, 6, 16));
 ballard.pushHourlySalesData(new HourlySalesData('1:00 am', 8, 15, 6, 16));
 
 //creating First Hill with constructor
-var firstHill = new PizzaLocation('First Hill');
+firstHill = new PizzaLocation('First Hill');
 //calling the pushHourlySalesData method with completed HourlySalesData constructor as data
 firstHill.pushHourlySalesData(new HourlySalesData('8:00 am', 1, 3, 1, 7));
 firstHill.pushHourlySalesData(new HourlySalesData('9:00 am', 1, 3, 1, 7));
@@ -64,7 +66,7 @@ firstHill.pushHourlySalesData(new HourlySalesData('12:00 am', 8, 20, 6, 16));
 firstHill.pushHourlySalesData(new HourlySalesData('1:00 am', 8, 20, 6, 16));
 
 //creating International District with constructor
-var international = new PizzaLocation('International District');
+international = new PizzaLocation('International District');
 //calling the pushHourlySalesData method with completed HourlySalesData constructor as data
 international.pushHourlySalesData(new HourlySalesData('8:00 am', 0, 4, 0, 4));
 international.pushHourlySalesData(new HourlySalesData('9:00 am', 0, 4, 0, 4));
@@ -86,29 +88,29 @@ international.pushHourlySalesData(new HourlySalesData('12:00 am', 0, 2, 2, 8));
 international.pushHourlySalesData(new HourlySalesData('1:00 am', 0, 2, 2, 8));
 
 //creating South Lake Union with constructor
-var slu = new PizzaLocation('South Lake Union');
+sLU = new PizzaLocation('South Lake Union');
 //calling the pushHourlySalesData method with completed HourlySalesData constructor as data
-slu.pushHourlySalesData(new HourlySalesData('8:00 am', 0, 4, 0, 4));
-slu.pushHourlySalesData(new HourlySalesData('9:00 am', 0, 4, 0, 4));
-slu.pushHourlySalesData(new HourlySalesData('10:00 am', 0, 4, 0, 4));
-slu.pushHourlySalesData(new HourlySalesData('11:00 am', 0, 7, 0, 4));
-slu.pushHourlySalesData(new HourlySalesData('12:00 pm', 0, 7, 0, 4));
-slu.pushHourlySalesData(new HourlySalesData('1:00 pm', 0, 7, 0, 4));
-slu.pushHourlySalesData(new HourlySalesData('2:00 pm', 5, 15, 0, 4));
-slu.pushHourlySalesData(new HourlySalesData('3:00 pm', 5, 15, 0, 4));
-slu.pushHourlySalesData(new HourlySalesData('4:00 pm', 5, 15, 0, 4));
-slu.pushHourlySalesData(new HourlySalesData('5:00 pm', 25, 39, 13, 18));
-slu.pushHourlySalesData(new HourlySalesData('6:00 pm', 25, 39, 13, 18));
-slu.pushHourlySalesData(new HourlySalesData('7:00 pm', 25, 39, 13, 18));
-slu.pushHourlySalesData(new HourlySalesData('8:00 pm', 22, 36, 5, 22));
-slu.pushHourlySalesData(new HourlySalesData('9:00 pm', 22, 36, 5, 22));
-slu.pushHourlySalesData(new HourlySalesData('10:00 pm', 22, 36, 5, 22));
-slu.pushHourlySalesData(new HourlySalesData('11:00 pm', 5, 21, 16, 31));
-slu.pushHourlySalesData(new HourlySalesData('12:00 am', 5, 21, 16, 31));
-slu.pushHourlySalesData(new HourlySalesData('1:00 am', 5, 21, 16, 31));
+sLU.pushHourlySalesData(new HourlySalesData('8:00 am', 0, 4, 0, 4));
+sLU.pushHourlySalesData(new HourlySalesData('9:00 am', 0, 4, 0, 4));
+sLU.pushHourlySalesData(new HourlySalesData('10:00 am', 0, 4, 0, 4));
+sLU.pushHourlySalesData(new HourlySalesData('11:00 am', 0, 7, 0, 4));
+sLU.pushHourlySalesData(new HourlySalesData('12:00 pm', 0, 7, 0, 4));
+sLU.pushHourlySalesData(new HourlySalesData('1:00 pm', 0, 7, 0, 4));
+sLU.pushHourlySalesData(new HourlySalesData('2:00 pm', 5, 15, 0, 4));
+sLU.pushHourlySalesData(new HourlySalesData('3:00 pm', 5, 15, 0, 4));
+sLU.pushHourlySalesData(new HourlySalesData('4:00 pm', 5, 15, 0, 4));
+sLU.pushHourlySalesData(new HourlySalesData('5:00 pm', 25, 39, 13, 18));
+sLU.pushHourlySalesData(new HourlySalesData('6:00 pm', 25, 39, 13, 18));
+sLU.pushHourlySalesData(new HourlySalesData('7:00 pm', 25, 39, 13, 18));
+sLU.pushHourlySalesData(new HourlySalesData('8:00 pm', 22, 36, 5, 22));
+sLU.pushHourlySalesData(new HourlySalesData('9:00 pm', 22, 36, 5, 22));
+sLU.pushHourlySalesData(new HourlySalesData('10:00 pm', 22, 36, 5, 22));
+sLU.pushHourlySalesData(new HourlySalesData('11:00 pm', 5, 21, 16, 31));
+sLU.pushHourlySalesData(new HourlySalesData('12:00 am', 5, 21, 16, 31));
+sLU.pushHourlySalesData(new HourlySalesData('1:00 am', 5, 21, 16, 31));
 
 //creating Georgetown with constructor
-var georgetown = new PizzaLocation('Georgetown');
+georgetown = new PizzaLocation('Georgetown');
 //calling the pushHourlySalesData method with completed HourlySalesData constructor as data
 georgetown.pushHourlySalesData(new HourlySalesData('8:00 am', 2, 7, 3, 5));
 georgetown.pushHourlySalesData(new HourlySalesData('9:00 am', 2, 7, 3, 5));
@@ -130,7 +132,7 @@ georgetown.pushHourlySalesData(new HourlySalesData('12:00 am', 15, 20, 6, 21));
 georgetown.pushHourlySalesData(new HourlySalesData('1:00 am', 15, 20, 6, 21));
 
 //creating Ravenna with constructor
-var ravenna = new PizzaLocation('Ravenna');
+ravenna = new PizzaLocation('Ravenna');
 //calling the pushHourlySalesData method with completed HourlySalesData constructor as data
 ravenna.pushHourlySalesData(new HourlySalesData('8:00 am', 0, 4, 0, 4));
 ravenna.pushHourlySalesData(new HourlySalesData('9:00 am', 0, 4, 0, 4));
@@ -150,3 +152,21 @@ ravenna.pushHourlySalesData(new HourlySalesData('10:00 pm', 4, 8, 2, 5));
 ravenna.pushHourlySalesData(new HourlySalesData('11:00 pm', 2, 4, 3, 11));
 ravenna.pushHourlySalesData(new HourlySalesData('12:00 am', 2, 4, 3, 11));
 ravenna.pushHourlySalesData(new HourlySalesData('1:00 am', 2, 4, 3, 11));
+
+//
+
+function generateRowData(inputArray){
+  var row = document.createElement('tr');
+  var col;
+  for (var i = 0; i < inputArray.length; i++){
+    col = document.createElement('td');
+    col.textContent = inputArray[i];
+    row.appendChild(col);
+  }
+  return row;
+}
+var ballardTable = document.getElementById('ballardTable');
+for (var i = 0; i < ballard.hourlySalesData.length; i++){
+  var someRow = generateRowData([ballard.hourlySalesData[i].time, ballard.hourlySalesData[i].pizzaSold, ballard.hourlySalesData[i].pizzaDelivered, ballard.hourlySalesData[i].driversNeeded]);
+  ballardTable.appendChild(someRow);
+}
